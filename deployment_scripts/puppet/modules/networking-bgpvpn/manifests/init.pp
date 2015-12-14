7137636587724561bf437e84295e0fb6f6f9aef1
Nikolas Hermanns <nikolas.hermanns@ericsson.com>
2015-12-14 18:16:26 +0100

    Add networking_odl as dependency
    Networking odl is needed cause it is a dependencie.
    There is no way yet to make the bgpvpn installation
    waiting for the ODL installation, when ODL
    installation is optional:
    http://permalink.gmane.org/gmane.comp.cloud.openstack.devel/63333
    
    Change-Id: I9971302b2a76bb7b1013e247821c35c16c6e7b40

--- deployment_scripts/puppet/modules/networking-bgpvpn/manifests/init.pp
+++ deployment_scripts/puppet/modules/networking-bgpvpn/manifests/init.pp
@@ -32,6 +32,9 @@ class networking-bgpvpn-backend-config {
     require networking-bgpvpn
     if hiera('opendaylight', false) {
         $NETWORKING_BGPVPN_DRIVER = 'BGPVPN:OpenDaylight:networking_bgpvpn.neutron.services.service_drivers.opendaylight.odl.OpenDaylightBgpvpnDriver:default'
+        package {'python-networking-odl':
+            ensure => installed,
+        }
     }
     else {
         fail('Bagpipe driver not yet included. You need to have anotehr bgpvpn dirver: Opendaylight')
