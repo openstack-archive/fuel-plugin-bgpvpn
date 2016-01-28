d52d4b112d25cda816ecff8e568d7157694f4cfd
Nikolas Hermanns <nikolas.hermanns@ericsson.com>
2016-01-28 19:29:05 +0100

    Uplift to Fuel8.0
    Change-Id: I6105c4e68763187846a1a3c1c09290950bb50796

--- deployment_scripts/puppet/modules/networking-bgpvpn/manifests/init.pp
+++ deployment_scripts/puppet/modules/networking-bgpvpn/manifests/init.pp
@@ -8,23 +8,6 @@ class networking-bgpvpn {
         ensure => installed,
         notify => Service['neutron-server'],
     }
-
-    # This is only needed for kilo.
-    # fuel-library/puppet/neutron/manifest/server.pp: exec 'neutron-db-sync' was taken as
-    # example. In liberty it is not needed
-    exec { 'bgpvpn-db-sync':
-      command     => 'touch /root/db.sync.done;bgpvpn-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugin.ini upgrade head',
-      path        => '/usr/local/bin:/usr/bin:/sbin:/bin:/usr/local/sbin:/usr/sbin',
-      notify      => Service['neutron-server'],
-      # TODO
-      #subscribe   => Neutron_config['database/connection'],
-      tries       => 10,
-      # TODO(bogdando) contribute change to upstream:
-      #   new try_sleep param for sleep driven development (SDD)
-      try_sleep   => 20,
-      require => Package["python-networking-bgpvpn"]
-    }
-    Exec<| title == 'neutron-db-sync' |> ~> Exec['bgpvpn-db-sync']
 }
 
 class networking-bgpvpn-backend-config {
@@ -40,5 +23,8 @@ class networking-bgpvpn-backend-config {
         fail('Bagpipe driver not yet included. You need to have anotehr bgpvpn dirver: Opendaylight')
     }
     # In liberty this goes to an own config file
-    neutron_config { 'service_providers/service_provider': value => $NETWORKING_BGPVPN_DRIVER;}
+    file { "/etc/neutron/networking_bgpvpn.conf":
+      ensure  => file,
+      content => template('networking-bgpvpn/networking_bgpvpn.conf'),
+    }
 }
