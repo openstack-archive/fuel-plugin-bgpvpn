e310599f65fff4ba58359b48993f7c039de60aff
Romanos Skiadas <rski@intracom-telecom.com>
2017-01-17 13:03:36 +0200

    Remove deployment time failure
    Make requiring the ODL plugin a condition to enable the BGPVPN plugin
    
    Change-Id: I655fd4645f03d20a9238a96b1db1f84ca9ec5f7e
    Signed-off-by: Romanos Skiadas <rski@intracom-telecom.com>

--- deployment_scripts/puppet/modules/networking-bgpvpn/manifests/init.pp
+++ deployment_scripts/puppet/modules/networking-bgpvpn/manifests/init.pp
@@ -34,10 +34,6 @@ class networking-bgpvpn-backend-config {
             ensure => installed,
         }
     }
-    else {
-        fail('Bagpipe driver not yet included. You need to have anotehr bgpvpn dirver: Opendaylight')
-    }
-    # In liberty this goes to an own config file
     file { "/etc/neutron/networking_bgpvpn.conf":
       ensure  => file,
       content => template('networking-bgpvpn/networking_bgpvpn.conf.erb'),
