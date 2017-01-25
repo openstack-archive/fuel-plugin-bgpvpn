35f89742897684018a7e5707c94bb436117ddad0
Thomas Sounapoglou <tomsou82@gmail.com>
2017-01-25 15:54:13 +0200

    Integrate OpenStack Horizon support for BGPVPN
    To integrate Hozizon to BGPVPN feature:
    - create links from plugin dashboard py files into horizon dashboard
    - restart apache
    
    Change-Id: I7740f9454cfbf8d076cf96e7bfade337090fb539

---
+++ deployment_scripts/puppet/manifests/horizon_integration.pp
@@ -0,0 +1,14 @@
+file { '/usr/share/openstack-dashboard/openstack_dashboard/local/enabled/_1495_project_bgpvpn_panel.py':
+    ensure => 'link',
+    target => '/usr/local/lib/python2.7/dist-packages/bgpvpn_dashboard/enabled/_1495_project_bgpvpn_panel.py',
+}
+->
+
+file { '/usr/share/openstack-dashboard/openstack_dashboard/local/enabled/_2115_admin_bgpvpn_panel.py':
+    ensure => 'link',
+    target => '/usr/local/lib/python2.7/dist-packages/bgpvpn_dashboard/enabled/_2115_admin_bgpvpn_panel.py',
+}
+~>
+service { 'apache2':
+    ensure => running,
+}
